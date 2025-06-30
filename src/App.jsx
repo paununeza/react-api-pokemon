b6ca0b1e161d3c1ca101221bb01654ddc57cbb2f
Paula <pnunezara@gmail.com>
2025-06-30 16:16:43 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PokemonFetcher from './PokemonFetcher';
+
+function App(){
+  return (
+    <>
+    <h1>¡Conoce a tus Pokemon!</h1>
+    <PokemonFetcher />
+    </>
+  );
+}
+
+export default App;
